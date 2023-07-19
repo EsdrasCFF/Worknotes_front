eac3468e1e4524e60a3a6bc55eb7dd39d0797e14
EsdrasCFF <ecff.dev@gmail.com>
2023-07-18 21:18:08 -0300

    Global Styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import GlobalStyles from './styles/global';
 
 import { ThemeProvider } from 'styled-components';
 
@@ -10,6 +11,7 @@ import { Details } from "./pages/Details";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme} >
+      <GlobalStyles />
       <Details />
     </ThemeProvider>
   </React.StrictMode>
