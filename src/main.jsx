c76dba68ffc030701c1e0f3b317f8c0c07bab6a7
EsdrasCFF <ecff.dev@gmail.com>
2023-07-23 16:51:39 -0300

    Style: Page SingIn Finished

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,13 @@ import { ThemeProvider } from 'styled-components';
 
 import theme from './styles/theme'
 
-import { Home } from "./pages/Home";
+import { SingIn } from "./pages/SingIn";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme} >
       <GlobalStyles />
-      <Home />
+      <SingIn />
     </ThemeProvider>
   </React.StrictMode>
 );
