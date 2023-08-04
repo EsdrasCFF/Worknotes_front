704805cf6228cd319033eb7c2568b7fadbab25e2
EsdrasCFF <ecff.dev@gmail.com>
2023-08-03 23:52:35 -0300

    Feat: Creating Authenticated Hook

--- src/main.jsx
+++ src/main.jsx
@@ -7,16 +7,15 @@ import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme'
 
 import { Routes } from "./routes";
-
-import MyContext from './myContext'
+import { AuthProvider } from './hooks/auth';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme} >
       <GlobalStyles />
-      <MyContext.Provider value={{Nome: Esdras, Email: esdras.email.com}}>
-        <Routes />
-      </MyContext.Provider>
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
