769157ddd750961e9e0bb597dffdbcb4c46aec44
EsdrasCFF <ecff.dev@gmail.com>
2023-08-08 23:25:30 -0300

    Feat: Implementing - update user

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -17,7 +17,7 @@ function AuthProvider({children}) {
     localStorage.setItem("@worknotes:token", token);
   
 
-    api.defaults.headers.authorization = `Bearer ${token}`;
+    api.defaults.headers.common["authorization"] = `Bearer ${token}`;
     setData({user, token})
       
     } catch(error) {
@@ -36,11 +36,34 @@ function AuthProvider({children}) {
     setData({});
   }
 
+  async function updateProfile({ user }) {
+    try{
+      await api.put("/users", user);
+      localStorage.setItem("@worknotes:user", JSON.stringify(user));
+
+      setData({
+        user,
+        token: data.token
+      })
+
+      alert("Success updated user")
+      
+    } catch(error) {
+      if(error.reponse) {
+        alert(error.reponse.data.message)
+      } else {
+        alert("Can't possible update user!")
+      }
+    }
+  }
+
   useEffect(() => {
     const user = localStorage.getItem("@worknotes:user");
     const token = localStorage.getItem("@worknotes:token");
 
     if(token && user) {
+      api.defaults.headers.common["authorization"] = `Bearer ${token}`;
+      
       setData({
         token, 
         user: JSON.parse(user)
@@ -49,7 +72,7 @@ function AuthProvider({children}) {
   }, [])
 
   return (
-    <AuthContext.Provider value={{ signIn, signOut, user: data.user }}>
+    <AuthContext.Provider value={{ signIn, signOut, updateProfile, user: data.user }}>
       {children}
     </AuthContext.Provider>
   )
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,10 +1,31 @@
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
 import { Container, Form, Avatar } from "./styles";
 
+import { useState } from 'react';
+import { useAuth } from '../../hooks/auth';
+
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
 export function Profile() {
+  const { user, updateProfile } = useAuth();
+
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+  const [passwordOld, setPasswordOld] = useState();
+  const [passwordNew, setPasswordNew] = useState();
+
+  async function handleUpdate() {
+    const user = {
+      name,
+      email,
+      password: passwordNew,
+      old_password: passwordOld
+    }
+
+    updateProfile({ user })
+  }
+
   return (
     <Container>
 
@@ -24,12 +45,37 @@ export function Profile() {
           </label>
         </Avatar>
 
-        <Input placeholder="Nome" type="text" icon={FiUser} ></Input>
-        <Input placeholder="E-mail" type="text" icon={FiMail} ></Input>
-        <Input placeholder="Senha Atual" type="password" icon={FiLock} ></Input>
-        <Input placeholder="Nova Senha" type="password" icon={FiLock} ></Input>
+        <Input 
+          placeholder="Nome" 
+          type="text" 
+          icon={FiUser}
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+
+        <Input 
+          placeholder="E-mail" 
+          type="text" 
+          icon={FiMail}
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+        
+        <Input 
+          placeholder="Senha Atual" 
+          type="password" 
+          icon={FiLock} 
+          onChange={e => setPasswordNew(e.target.value)}
+        />
+        
+        <Input 
+          placeholder="Nova Senha" 
+          type="password" 
+          icon={FiLock}
+          onChange={e => setPasswordOld(e.target.value)}
+        />
       
-        <Button title="Salvar" />
+        <Button title="Salvar" onClick={handleUpdate} />
       
       </Form>
 
