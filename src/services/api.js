9bdaa7bac6147b9677410dcc0fdd6d36175e3580
Esdras Castro <ecff.dev@gmail.com>
2023-08-29 21:15:23 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://writenotes-api-5caf19a80683.herokuapp.com/"
+  baseURL: "https://writenotes-api-5caf19a80683.herokuapp.com"
 });
